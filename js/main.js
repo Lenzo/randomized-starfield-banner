c1bfd031a13de16890da0be5488d278c6404fb2c
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-04-08 06:54:11 -0400

    everything but anymation working

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-var $banner = ('.banner');
+var $banner = $('.banner');
 
 var makeStar = function () {
-  var $newStar = $('<img src="images/star.svg">');
+  var $newStar = $('<div>');
   var $newTrans = Math.random();
   var $newSize = Math.random() * 20;
+  var $newRotate = Math.random() * 360;
 
-
+  $newStar.addClass('star');
   $newStar.css('top', Math.random() * document.documentElement.clientHeight - 22);
   $newStar.css('left', Math.random() * document.documentElement.clientWidth - 22);
   $newStar.css('opacity', $newTrans);
   $newStar.css('width', $newSize);
+  $newStar.css( 'transform', 'rotate(' + $newRotate + 'deg)');
+  $newStar.addClass('animateStar');
 
   $banner.append($newStar);
 };
